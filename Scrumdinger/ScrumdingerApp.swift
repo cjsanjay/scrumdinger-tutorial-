7ea99692eecab1a1129786f84ebc21f026a1dc97
Sanjay Singh <sanjaysi@usc.edu>
2023-10-08 20:00:52 +0530

    Adding all the initial files

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Sanjay Singh on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
